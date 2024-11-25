8ac13629dc708776fa90a4abebf8dda7c18ef2ff
Zyndrr <erin.d.mckanna@gmail.com>
2024-11-24 22:31:49 -0700

    add setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')!).render(
-    <App />
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
+    children: []
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
 )
