67959ceea3b0cce1384645f3937debe71f0e02f8
Devin Sharp <sharpdevin467@gmail.com>
2024-12-05 22:14:45 -0800

    add buttons

--- client/src/components/menu_page.tsx
+++ client/src/components/menu_page.tsx
@@ -7,14 +7,27 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
 import { useEffect, useState } from 'react';
-import { Button, Container, Stack } from '@mui/material';
+import { Button, Container, Modal, Stack, TextareaAutosize, Typography } from '@mui/material';
 import { Router, useNavigate } from 'react-router-dom';
+import MenuNameModal from './menuNameModal';
+
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 export default function MenuPage() {
     const navigate = useNavigate();
 
     //currently this does not do what we want. Will need to modify all of this based on how we setup the drink data.
-    const [selectedIndex, _setSelectedIndex] = React.useState(1);
 
     //deletes the associated drink from the users menu
     const handleListItemClick = async (
@@ -26,9 +39,10 @@ export default function MenuPage() {
     };
 
     //Prompts the user to enter a new name for their menu, sets that in their account, and then reloads
-    const renameMenuOpen = async (
-        _event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
+    const submitMenuName = async () => {
+        setOpen(false);
+        await fetch(`/api/user/drinks/menu`, {method: "PATCH", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ token: localStorage.getItem("jwt"), menuName: name }) });
+        window.location.reload();
     }
 
     //deletes all the drinks from the user's menu and then reloads
@@ -46,6 +60,8 @@ export default function MenuPage() {
     }
 
     const [data, setData] = useState({ menuName: "", drinks: [{ _id: "", name: "", ingredients: [] }] });
+    const [open, setOpen] = useState(false);
+    const [name, setName] = useState("");
 
     //pulls the user's drinks and menu name to be populated on the page
     useEffect(() => {
@@ -55,6 +71,7 @@ export default function MenuPage() {
                 const response = await fetch(`/api/user/${userToken}`);
                 const jsonData = await response.json();
                 setData(jsonData);
+                setName(jsonData.menuName);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -66,8 +83,8 @@ export default function MenuPage() {
     return (
         <>
             <Box>
-                    <Button variant="contained" onClick={toSearchByName}>Add Drink By Name</Button>
-                    <Button variant="contained" onClick={toSearchByIngredients}>Add Drink By Ingredients</Button>
+                <Button variant="contained" onClick={toSearchByName}>Add Drink By Name</Button>
+                <Button variant="contained" onClick={toSearchByIngredients}>Add Drink By Ingredients</Button>
             </Box>
             <Box
                 sx={{
@@ -76,8 +93,15 @@ export default function MenuPage() {
                     border: 10,
                 }}
             >
-                <Button variant="contained" onClick={(e) => renameMenuOpen(e)}>Rename menu</Button>
+                <Button variant="contained" onClick={() => deleteFullMenuButton()}>Rename menu</Button>
                 <h2>{data.menuName}</h2>
+                <Button onClick={() => setOpen(!open)}>Open modal</Button>
+                <Modal open={open} onClose={submitMenuName} closeAfterTransition>
+                        <Box sx={modalStyle}>
+                            <Typography>Enter your new menu name</Typography>
+                            <TextareaAutosize onChange={(e)=>setName(e.currentTarget.value)}></TextareaAutosize>
+                        </Box>
+                </Modal>
                 {data.drinks && data.drinks.map(drink => (
                     <List sx={{ width: "90%", height: "90%", textAlign: 'center' }}>
                         <ListItem>
