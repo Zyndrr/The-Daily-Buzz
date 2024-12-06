fdd6dffa00589bacb0c05c558e76f9ed263d2cc4
Devin Sharp <sharpdevin467@gmail.com>
2024-12-05 21:22:44 -0800

    add buttons to page

--- client/src/components/menu_page.tsx
+++ client/src/components/menu_page.tsx
@@ -7,40 +7,68 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
 import { useEffect, useState } from 'react';
+import { Button, Container, Stack } from '@mui/material';
+import { Router, useNavigate } from 'react-router-dom';
 
 export default function MenuPage() {
+    const navigate = useNavigate();
 
-        //currently this does not do what we want. Will need to modify all of this based on how we setup the drink data.
-        const [selectedIndex, _setSelectedIndex] = React.useState(1);
+    //currently this does not do what we want. Will need to modify all of this based on how we setup the drink data.
+    const [selectedIndex, _setSelectedIndex] = React.useState(1);
 
-        // const handleListItemClick = (
-        //     _event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        //     index: number,
-        // )  => {
-        //     let path = `/details/${data[index].symbol}`;
-        //     window.location.href = path;
-        // };
-    
-        const [data, setData] = useState({menuName: "", drinks: [{_id: "", name: "", ingredients: []}] });
-    
-        useEffect(() => {
-          const fetchData = async () => {
+    //deletes the associated drink from the users menu
+    const handleListItemClick = async (
+        _event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        drinkId: string,
+    ) => {
+        await fetch(`/api/user/drinks/${drinkId}`, { method: "DELETE", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ token: localStorage.getItem("jwt") }) });
+        window.location.reload();
+    };
+
+    //Prompts the user to enter a new name for their menu, sets that in their account, and then reloads
+    const renameMenuOpen = async (
+        _event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+    }
+
+    //deletes all the drinks from the user's menu and then reloads
+    const deleteFullMenuButton = async () => {
+        await fetch('/api/user/drinks/', { method: "DELETE", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ token: localStorage.getItem("jwt") }) });
+        window.location.reload();
+    }
+
+    const toSearchByIngredients = () => {
+        navigate("/search-ingredient");
+    }
+
+    const toSearchByName = () => {
+        navigate("/search-name");
+    }
+
+    const [data, setData] = useState({ menuName: "", drinks: [{ _id: "", name: "", ingredients: [] }] });
+
+    //pulls the user's drinks and menu name to be populated on the page
+    useEffect(() => {
+        const fetchData = async () => {
             try {
                 const userToken = localStorage.getItem("jwt");
                 const response = await fetch(`/api/user/${userToken}`);
                 const jsonData = await response.json();
-              //console.log("fetched data or sumthin", jsonData);
-              setData(jsonData);
+                setData(jsonData);
             } catch (error) {
-              console.error('Error fetching data:', error);
+                console.error('Error fetching data:', error);
             }
-          };
-      
-          fetchData();
-        }, []);
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <>
+            <Box>
+                    <Button variant="contained" onClick={toSearchByName}>Add Drink By Name</Button>
+                    <Button variant="contained" onClick={toSearchByIngredients}>Add Drink By Ingredients</Button>
+            </Box>
             <Box
                 sx={{
                     width: '100%',
@@ -48,16 +76,18 @@ export default function MenuPage() {
                     border: 10,
                 }}
             >
-            <h2>access the menu name here and display</h2>
+                <Button variant="contained" onClick={(e) => renameMenuOpen(e)}>Rename menu</Button>
+                <h2>{data.menuName}</h2>
                 {data.drinks && data.drinks.map(drink => (
                     <List sx={{ width: "90%", height: "90%", textAlign: 'center' }}>
-                        <ListItemButton
-                            selected={selectedIndex === 0}
-                            onClick={(event) => handleListItemClick(event, 0)}
-                        >
-                            <ListItemText>{drink.name}</ListItemText>
+                        <ListItem>
+                            <ListItemText sx={{ alignItems: "center" }}>{drink.name}</ListItemText>
                             <ListItemText>Ingredients: {drink.ingredients.join(", ")}</ListItemText>
-                        </ListItemButton>
+                            <ListItemButton
+                                onClick={(event) => handleListItemClick(event, drink._id)}
+                            >Remove
+                            </ListItemButton>
+                        </ListItem>
                     </List>
                 )
                 )}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Login from "./components/login.js";
+import Login from "./components/Login.js";
 import Home from "./components/Home.js";
 import SignUp from "./components/SignUp.js";
 import MenuPage from "./components/menu_page.js";
