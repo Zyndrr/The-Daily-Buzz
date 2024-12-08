514532b6fe4ecc048de61f69fb121d6d5f3b042f
Devin Sharp <sharpdevin467@gmail.com>
2024-12-08 14:52:26 -0800

    fixing render routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import db from "./config/connection.js";
 import routes from "./routes/index.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +16,10 @@ app.use(express.json());
 app.use(routes);
 
 await db();
+
+app.get('/*', function (_req, res) {
+  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+}); 
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
